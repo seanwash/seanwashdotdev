8a1e2cc0f0675d54d7d8013b6035c3c20c267fc9
Sean Washington <hello@seanwash.com>
2022-07-22 12:06:48 -0700

    Make the matter index mobile friendly

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -5,7 +5,7 @@
         x-ref="filterForm"
         action="{{ route('admin.home') }}"
         method="GET"
-        class="flex items-end space-x-2"
+        class="md:flex items-end space-y-2 md:space-y-0 md:space-x-2"
     >
         <div class="w-full">
             <label for="keyword">Search</label>
@@ -39,58 +39,60 @@
         </div>
 
         <div class="space-x-2">
-            <button type="submit">Filter</button>
+            <button type="submit" class="w-full">Filter</button>
         </div>
     </form>
 
-    <table>
-        <thead>
-            <tr>
-                <th>Type</th>
-                <th>Name</th>
-                <th>Tags</th>
-                <th>Public At</th>
-                <th class="text-right">Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($matter as $matter_item)
+    <section class="overflow-auto">
+        <table>
+            <thead>
                 <tr>
-                    <td>
-                        <x-tag>{{ $matter_item->type }}</x-tag>
-                    </td>
-                    <td>
-                        @if($matter_item->external_url)
-                            <a href="{{ $matter_item->external_url }}">{{ $matter_item->name }}</a>
-                        @else
-                            {{ $matter_item->name }}
-                        @endif
-                    </td>
-                    <td>
-                        <x-tag-list>
-                            @foreach($matter_item->tags as $tag)
-                                <x-tag>{{ $tag->name }}</x-tag>
-                            @endforeach
-                        </x-tag-list>
-                    </td>
-                    <td>
-                        {{ $matter_item->public_at->toDateString() }}
-                    </td>
-                    <td class="text-right">
-                        <a href="{{ route('admin.matter.edit', $matter_item) }}"
-                           class="text-sm inline-block ml-2">Edit</a>
-
-                        <form action="{{ route('admin.matter.destroy', $matter_item) }}" method="POST"
-                              class="inline-block ml-2">
-                            @method('delete')
-                            @csrf
-                            <button type="submit" class="p-0 underline text-sm !text-black !bg-transparent">Delete
-                            </button>
-                        </form>
-                    </td>
+                    <th>Type</th>
+                    <th>Name</th>
+                    <th>Tags</th>
+                    <th>Public At</th>
+                    <th class="text-right">Actions</th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                @foreach($matter as $matter_item)
+                    <tr>
+                        <td>
+                            <x-tag>{{ $matter_item->type }}</x-tag>
+                        </td>
+                        <td class="whitespace-nowrap max-w-sm">
+                            @if($matter_item->external_url)
+                                <a href="{{ $matter_item->external_url }}">{{ $matter_item->name }}</a>
+                            @else
+                                {{ $matter_item->name }}
+                            @endif
+                        </td>
+                        <td>
+                            <x-tag-list>
+                                @foreach($matter_item->tags as $tag)
+                                    <x-tag>{{ $tag->name }}</x-tag>
+                                @endforeach
+                            </x-tag-list>
+                        </td>
+                        <td class="whitespace-nowrap">
+                            {{ $matter_item->public_at->toDateString() }}
+                        </td>
+                        <td class="text-right whitespace-nowrap">
+                            <a href="{{ route('admin.matter.edit', $matter_item) }}"
+                               class="text-sm inline-block ml-2">Edit</a>
+
+                            <form action="{{ route('admin.matter.destroy', $matter_item) }}" method="POST"
+                                  class="inline-block ml-2">
+                                @method('delete')
+                                @csrf
+                                <button type="submit" class="p-0 underline text-sm !text-black !bg-transparent">Delete
+                                </button>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </section>
 
 </x-admin.layout>
